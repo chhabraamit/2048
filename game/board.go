2e708a9e880d7ec4939ce2c344a92b33a4e35389
Amit Chhabra <chhabra@uber.com>
2020-09-11 00:52:41 +0530

    Display board with random numbers

---
+++ game/board.go
@@ -0,0 +1,72 @@
+package game
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+const rows = 4
+const cols = 4
+
+type Board interface {
+	Display()
+}
+
+type board struct {
+	matrix [][]int
+}
+
+func (b board) Display() {
+	b.matrix = getRandom()
+	printHorizontal()
+	for i := 0; i < len(b.matrix); i++ {
+		printVertical()
+		for j := 0; j < len(b.matrix[0]); j++ {
+			fmt.Printf("%6d", b.matrix[i][j])
+		}
+		fmt.Printf("%4s", "")
+		printVertical()
+		fmt.Println()
+	}
+	printHorizontal()
+}
+
+func printVertical() {
+	fmt.Print("|")
+}
+
+func printHorizontal() {
+	for i := 0; i < 30; i++ {
+		fmt.Print("-")
+	}
+	fmt.Println()
+}
+
+func getRandom() [][]int {
+	arr := make([]int, 0)
+	val := 2
+	arr = append(arr, val)
+	for true {
+		val *= 2
+		arr = append(arr, val)
+		if val == 2048 {
+			break
+		}
+	}
+	size := len(arr)
+	board := make([][]int, 0)
+	for i := 0; i < 4; i++ {
+		board = append(board, make([]int, 4))
+	}
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			element := arr[rand.Int()%size]
+			board[i][j] = element
+		}
+	}
+	return board
+}
+
+func New() Board {
+	return &board{}
+}
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/chhabra/2048/game"
+)
 
 func main() {
 	fmt.Printf("Getting started\n")
+	g := game.New()
+	g.Display()
 }
